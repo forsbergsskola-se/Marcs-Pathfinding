b4868278801eb6f4b32bdd95d6588325582be73f
augustlewen <augustlewen@live.com>
2024-05-20 16:15:35 +0200

    updated gridinspector

--- Assets/Scripts/Editor/GridInspector.cs
+++ Assets/Scripts/Editor/GridInspector.cs
@@ -1,18 +1,50 @@
+using DefaultNamespace;
 using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(Grid))]
 public class GridInspector : Editor
 {
+
+    private Object cellPrefab;
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
 
+        cellPrefab = EditorGUILayout.ObjectField("Cell Prefab", cellPrefab, typeof(GridCell));
+        GUI.enabled = cellPrefab != null;
+
         if (GUILayout.Button("Generate Grid"))
         {
             Grid grid = target as Grid;
+
+            if (EditorUtility.IsDirty(grid))
+            {
+                foreach (var cell in grid.gridCells)
+                {
+                    if(cell != null)
+                        DestroyImmediate(cell.gameObject);
+                }
+            }
             
             EditorUtility.SetDirty(grid);
+            GenerateCells(grid);
         }
+        GUI.enabled = true;
+    }
+    
+    private void GenerateCells(Grid grid)
+    {
+        int index = 0;
+        
+        for(int y = 0; y < (grid.gridCells.Length) / grid.width; y++)
+            for (int x = 0; x < grid.width; x++)
+            {
+                var cell = (GridCell)PrefabUtility.InstantiatePrefab(cellPrefab);
+                cell.transform.position = new Vector3(x, y);
+                cell.name = "GridCell{" + x + ", " + y + "]";
+                grid.gridCells[index] = cell;
+                index++;
+            }
     }
 }
