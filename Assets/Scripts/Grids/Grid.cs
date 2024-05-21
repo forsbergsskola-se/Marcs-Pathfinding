c20b8f2e867c3620b6d47eea4d3d95cb90827581
augustlewen <augustlewen@live.com>
2024-05-21 15:48:54 +0200

    did more stuff

--- Assets/Scripts/Grids/Grid.cs
+++ Assets/Scripts/Grids/Grid.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Grids
@@ -6,23 +8,56 @@ namespace Grids
     {
         public GridCell[] gridCells = new GridCell[100];
         public int width = 10;
-
+        public int height = 10;
     
-    
-        public GridCell GetGridCell(int x, int y)
+        public GridCell GetGridCellFromIndex(Vector2Int cellPos)
         {
-            Debug.Log(x + " " + y);
-            return gridCells[y * width + x];
+            return gridCells[cellPos.y * width + cellPos.x];
         }
-
+        public Vector2Int GetCellIndexForPosition(Vector3 position)
+        {
+            return new Vector2Int(Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y));
+        }
+        
         public GridCell GetCellFromPosition(Vector3 position)
         {
-            return GetGridCell(Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y));
+            return GetGridCellFromIndex(GetCellIndexForPosition(position));
         }
-    
-        public bool isWalkable(int x, int y)
+        
+        bool IsValidAndWalkable(Vector2Int index)
         {
-            return GetGridCell(x, y).isWalkable;
+            if (index.x < 0) return false;
+            if (index.x >= width) return false;
+            if (index.y < 0) return false;
+            if (index.y >= height) return false;
+            return isWalkable(index);
+        }
+
+
+        public IEnumerable<Vector2Int> GetWalkDirections()
+        {
+            yield return Vector2Int.up;
+            yield return Vector2Int.right;
+            yield return Vector2Int.down;
+            yield return Vector2Int.left;
+        }
+        
+        
+        public IEnumerable<GridCell> GetWalkableNeighboursForCell(GridCell cell)
+        {
+            var cellIndex = GetCellIndexForPosition(cell.transform.position);
+
+            foreach (var direction in GetWalkDirections())
+            {
+                if (IsValidAndWalkable(cellIndex + direction))
+                    yield return GetGridCellFromIndex(cellIndex + direction);
+            }
+        }
+
+
+        public bool isWalkable(Vector2Int gridpos)
+        {
+            return GetGridCellFromIndex(gridpos).isWalkable;
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,13 +14,48 @@ public class PlayerController : MonoBehaviour
         {
             Grid grid = FindObjectOfType<Grid>();
             var path = FindPath(grid, grid.GetCellFromPosition(transform.position), grid.GetCellFromPosition(goal.position));
+            
+            foreach (var node in path)
+            {
+                node.spriteRenderer.color = Color.green;
+            }
+            
         }
     }
 
 
     static IEnumerable<GridCell> FindPath(Grid grid, GridCell start, GridCell end)
     {
+        Stack<GridCell> path = new Stack<GridCell>();
+        HashSet<GridCell> visited = new HashSet<GridCell>();
+        
+        path.Push(start);
+        visited.Add(start);
 
-        return new GridCell[5];
+        while (path.Count > 0)
+        {
+            bool foundNextNode = false;
+            foreach (var neighbour in grid.GetWalkableNeighboursForCell(path.Peek()))
+            {
+                if(visited.Contains(neighbour)) 
+                    continue;
+                
+                path.Push(neighbour);
+                visited.Add(neighbour);
+                neighbour.spriteRenderer.color = Color.cyan;
+                
+                if (neighbour == end)
+                    return path;
+
+                foundNextNode = true;
+                break;
+            }
+
+            if (!foundNextNode)
+                path.Pop();
+        }
+        
+        
+        return null;
     }
 }
