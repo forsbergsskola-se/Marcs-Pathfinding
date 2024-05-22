0201681cba43a154224e8c29ac0da47e344ec9bf
augustlewen <augustlewen@live.com>
2024-05-22 13:27:15 +0200

    updated grid cell to add celltypes

--- Assets/Scripts/Grids/GridCell.cs
+++ Assets/Scripts/Grids/GridCell.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Grids
@@ -6,11 +7,39 @@ namespace Grids
     public class GridCell : MonoBehaviour
     {
         public SpriteRenderer spriteRenderer;
-        public bool isWalkable;
 
+        [SerializeField] private CellType cellType;
+        public enum CellType
+        {
+            Ground,
+            Wall,
+            Water
+        }
+
+        public bool isWalkable => cellType != CellType.Wall;
+
+        public int Cost =>
+                cellType switch
+                {
+                    CellType.Ground => 1,
+                    CellType.Wall => int.MaxValue,
+                    CellType.Water => 2,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+        
+        
         private void Start()
         {
             OnValidate();
+        }
+
+        public override string ToString()
+        {
+            int xPos = Mathf.RoundToInt(transform.position.x);
+            int yPos = Mathf.RoundToInt(transform.position.y);
+            
+            return
+                $"{nameof(GridCell)} ({xPos}|{yPos}";
         }
 
         public void OnValidate()
